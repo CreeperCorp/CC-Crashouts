3de87c2116816d21d0de935c1641190b8c51a952
CreepSplotion <dekaiyondavis8@gmail.com>
2025-12-04 21:16:27 -0500

    Add functionality to display CC Crashout winners

--- code.py
+++ code.py
@@ -1,1 +1,7 @@
-
+ccWinners = input("Do you want to see everyone who has won CC Crashouts?(yes/no): ")
+if ccWinners == "yes":
+  print("CreepSplotion: CC Crashouts S1")
+  print("P0ly: CC Loser Crashouts")
+  print("Finxlly: CC Double Loser Crashouts")
+  print("Sharpness: CC Triple Loser Crashouts")
+  print("Ennard: CC Crashouts S2")
